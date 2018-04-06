3f1b832999ac2a7862a9a46d771cddcc3c999ff0
Caroline Antonello <caroles.antonello@gmail.com>
2018-04-06 09:28:24 -0300

    Listando nome dos veículos em uma lista e apresentando na tela inicial do aplicativo

---
+++ TesteDrive/TesteDrive/TesteDrive/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TesteDrive
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+	public partial class MainPage : ContentPage
+	{
+        public List<Veiculo> Veiculos { get; set; }
+
+        public MainPage()
+		{
+			InitializeComponent();
+
+            ListViewVeiculos.ItemsSource = new string[]
+            {
+                "Azera V6",
+                "Fiesta 2.0",
+                "HB20 S",
+                "Prisma LZT",
+                "Saveiro Cross",
+                "Amarock",
+                "S10",
+                "Golf esport",
+                "Classic",
+                "Strada",
+                "Sonic"
+            };
+
+		}
+	}
+}
