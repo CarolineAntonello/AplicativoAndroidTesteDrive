4ee9ae712dd6f34efbfc2bdaf93e91644562526f
Caroline Antonello <caroles.antonello@gmail.com>
2018-04-07 12:40:39 -0300

    Utilização de margem na tela inicial e mensagem ao selecionar um modelo de veículo

--- TesteDrive/TesteDrive/TesteDrive/MainPage.xaml.cs
+++ TesteDrive/TesteDrive/TesteDrive/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace TesteDrive
     {
         public string Nome { get; set; }
         public decimal Preco { get; set; }
+        public string PrecoFormatado { get { return string.Format("R$ {0}", Preco); } }
     }
 
 	public partial class MainPage : ContentPage
@@ -21,21 +22,35 @@ namespace TesteDrive
 		{
 			InitializeComponent();
 
-            ListViewVeiculos.ItemsSource = new string[]
+            this.Veiculos = new List<Veiculo>
             {
-                "Azera V6",
-                "Fiesta 2.0",
-                "HB20 S",
-                "Prisma LZT",
-                "Saveiro Cross",
-                "Amarock",
-                "S10",
-                "Golf esport",
-                "Classic",
-                "Strada",
-                "Sonic"
+                new Veiculo{Nome = "Azera V6", Preco = 60000},
+                new Veiculo{Nome = "Fiesta 2.0", Preco = 50000},
+                new Veiculo{Nome = "HB20 S", Preco = 40000},
+                new Veiculo{Nome = "Prisma LZT", Preco = 60000},
+                new Veiculo{Nome = "Saveiro Cross", Preco = 40000},
+                new Veiculo{Nome = "Amarock", Preco = 115000},
+                new Veiculo{Nome = "S10", Preco = 120000},
+                new Veiculo{Nome = "Golf esport", Preco = 90000},
+                new Veiculo{Nome = "Strada", Preco = 45000},
+                new Veiculo{Nome = "Classic", Preco = 30000},
+                new Veiculo{Nome = "Prisma LZT", Preco = 60000},
+                new Veiculo{Nome = "Saveiro Cross", Preco = 40000},
+                new Veiculo{Nome = "Amarock", Preco = 115000},
+                new Veiculo{Nome = "S10", Preco = 120000},
+                new Veiculo{Nome = "Golf esport", Preco = 90000},
+                new Veiculo{Nome = "Strada", Preco = 45000},
+                new Veiculo{Nome = "Classic", Preco = 30000}
             };
 
+            this.BindingContext = this;
 		}
-	}
+
+        private void ListViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+            DisplayAlert("Teste Drive", string.Format("Você tocou no modelo '{0}', que custa {1}",veiculo.Nome,veiculo.PrecoFormatado), "OK");
+        }
+
+    }
 }
